293b91f13ee46b4ea79dce4428247f2c1326eb30
tRexSTYT <egor.grachev.mail@gmail.com>
2019-03-11 20:41:35 +0300

    Добавлен код 2 задания;

---
+++ ProjectAssignment_1/main.c
@@ -0,0 +1,19 @@
+/*
+Проектное задание №1
+
+@author Eg0r.Grachev
+@link https://github.com/tRexSTYT/c-code
+@link http://moria.1586.su/moodle/mod/page/view.php?id=1709
+*/
+#include <stdio.h>
+#include <iostream>
+
+/*
+@return Возвращает код ошибки в систему;
+@throws | *error;
+*/
+int main() {
+    printf("Your mom gay :)\n");
+    system("pause");
+    return 0;
+}
