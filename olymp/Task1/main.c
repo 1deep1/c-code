1a14040e8066e07a3176861031139882a88058a6
deep <egor.grachev.mail@gmail.com>
2019-10-22 22:21:18 +0300

    Решена 1 олимпиадная задача

--- olymp/Task1/main.c
+++ olymp/Task1/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
 int main() {
-    int h, a, b;
+    int h, a, b, c;
+
+    scanf("%d\n%d\n%d", &h, &a, &b);
+    c = h - a + b;
+    if (c >= 24) c-=24;
+    if (c < 0) c+=24;
+    printf("%d", c);
     return 0;
 }
