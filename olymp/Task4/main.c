cf773eb19e01b8b9898f976993d06fcf7ebe3ce4
deep <egor.grachev.mail@gmail.com>
2019-10-23 16:15:35 +0300

    Решение 4 олимпиадной задачи

--- olymp/Task4/main.c
+++ olymp/Task4/main.c
@@ -2,10 +2,22 @@
 #include <string.h>
 
 int main() {
-    int a, b, c = 0;
-
-    scanf("%d\n%d", &a, &b);
-
-    printf("%d", c);
-    return 0;
+    char S[100001];
+	int len;
+	scanf("%s", S);
+	len = strlen(S);
+	for (int i = 0; i < len; i++) {
+		if(S[i] != S[len - 1 - i]) {
+			if(S[i] == S[len - 2 - i]) {
+				printf("%d", len-i);
+				break;
+			}
+			if(S[i+1] == S[len - 1 - i]) {
+				printf("%d", i);
+				break;
+			}
+				
+		}
+	}
+	return 0;
 }
