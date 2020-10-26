4e19339dd3550450df554b47d82585a27e89c73c
deep <egor.grachev.mail@gmail.com>
2020-10-26 15:41:52 +0300

    fixing ht2

--- mirea_code/ht2/4task/main.cpp
+++ mirea_code/ht2/4task/main.cpp
@@ -1,9 +1,36 @@
 #include <iostream>
-#include <stdlib.h>
-#include <time.h>
+
+#define A_LENGTH    9
 
 using namespace std;
 
 int main() {
+    int N, A[A_LENGTH];
+
+    int error = 0;
+    char key = 'y';
+
+    cout << "Привет! Давай поиграем в армию. Числа в ряд от N становись!\n";
+
+    while (key == 'Y' || key == 'y') {
+        cout << "Введите N: ";
+        cin >> N;
+        cout << "\nПоследовательность от " << N << ": ";
+
+        if (N < -9) {
+            cout << "Прости, не моху.";
+        }
+        else {
+            for (int i = 1; i <= A_LENGTH + 1; i++) {
+                A[i] = N + i;
+                cout << A[i] << " ";
+            }
+            cout << ";";
+        }
+
+        cout << "\n\nХочешь еще разок? (Y или N): ";
+        cin >> key;
+    }
+
     return 0;
 }
---
+++ mirea_code/ht2/5task/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float x = -4.0, y, step = 0.5;
+
+    int error = 0;
+    char key = 'y';
+
+    cout << "Привет! Давай протабулируем функцию y.\n";
+
+    while (key == 'Y' || key == 'y') {
+        for (int i = 0; i < 16; i++) {
+            x += step;
+            cout << x << endl;
+        }
+
+        cout << "\nХочешь еще разок? (Y или N): ";
+        cin >> key;
+    }
+
+    return 0;
+}
