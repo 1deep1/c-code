9663b821b2551d4735d5b57e9fc043e449b7af5d
deep <egor.grachev.mail@gmail.com>
2020-11-09 13:22:49 +0300

    added ht2 5 task

--- mirea_code/ht2/5task/main.cpp
+++ mirea_code/ht2/5task/main.cpp
@@ -3,9 +3,7 @@
 using namespace std;
 
 int main() {
-    setlocale(LC_ALL, "rus");
-
-    float x = -4.0, y, step = 0.5;
+    float x = -4.5, y, step = 0.5;
 
     int error = 0;
     char key = 'y';
@@ -13,9 +11,11 @@ int main() {
     cout << "Привет! Давай протабулируем функцию y.\n";
 
     while (key == 'Y' || key == 'y') {
-        for (int i = 0; i < 16; i++) {
+        for (int i = 0; i < 17; i++) {
             x += step;
-            cout << x << endl;
+            if (x - 1 != 0) y = (x*x - 2*x + 2) / (x - 1);
+            else cout << "X = " << x << "\nY = (нельзя делить на ноль)\n\n";
+            cout << "X = " << x << "\nY = " << y << "\n\n";
         }
 
         cout << "\nХочешь еще разок? (Y или N): ";
