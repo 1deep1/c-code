4deacf4e0e075ff6c7035dd58a43049cea152b02
deep <egor.grachev.mail@gmail.com>
2021-03-29 15:26:30 +0300

    Started working on the 4 practice

--- mirea_code/1-course_2-semester_dataStructuresAndAlgorithms/Practice4/main.cpp
+++ mirea_code/1-course_2-semester_dataStructuresAndAlgorithms/Practice4/main.cpp
@@ -1,7 +1,35 @@
 #include <iostream>
+//#include "test.cpp"
 
 using namespace std;
 
+int testFunc() {
+    cout << "\n(0) - alg_name\n";
+    return 0;
+}
+
 int main() {
+    bool sortChecker;
+    char key = 'y';
+
+    cout << "Grachev E., Practice 4;\n" << endl << "Hey! Wanna see some sort? Choose an algorithm:";
+
+    while (key == 'Y' || key == 'y') {
+        cout << "\n(0) - alg_name" << endl << "(1) - alg_name" << endl;
+        cout << "\nYour choice: ";
+        cin >> sortChecker;
+
+        switch (sortChecker) {
+            case false:
+                testFunc();
+                break;
+            case true:
+                cout << "\nTHX!\n";
+                break;
+        }
+        cout << "\nOnce again? (Y or N): ";
+        cin >> key;
+    }
+
     return 0;
 }
